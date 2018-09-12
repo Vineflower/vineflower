7d444df2afe37389d9936eebf00cc64012f420a6
LexManos <LexManos@gmail.com>
2018-09-12 03:01:05 -0700

    Prioritize self and enclosing class when encountering inconsistent InnerClass attributes.
    The compiler encodes all REFERENCED inner classes into the class. The first found used to win, but now ThisClass > EnclosingClass > Others AccessTransformers only edit the targeted class as it can't find all references.
    Fixes AccessTransformers.

--- src/org/jetbrains/java/decompiler/main/ClassWriter.java
+++ src/org/jetbrains/java/decompiler/main/ClassWriter.java
@@ -1219,6 +1219,10 @@ public class ClassWriter {
     }
   }
 
+  public static String getModifiers(int flags) {
+    return MODIFIERS.entrySet().stream().filter(e -> (e.getKey() & flags) != 0).map(Map.Entry::getValue).collect(Collectors.joining(" "));
+  }
+
   public static void appendTypeParameters(TextBuffer buffer, List<String> parameters, List<List<VarType>> bounds) {
     buffer.append('<');
 
