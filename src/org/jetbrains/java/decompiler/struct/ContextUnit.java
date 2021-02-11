5f3502eb429de8c185888f19c1f918089c4cc204
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-02-12 00:07:47 +0100

    [java decompiler] minor refactoring + cleanup (duplicates; dependencies; test data layout; typos; formatting)
    GitOrigin-RevId: 3589e4d8f2dfa8a5096fcf49070bc65ba6734482

--- src/org/jetbrains/java/decompiler/struct/ContextUnit.java
+++ src/org/jetbrains/java/decompiler/struct/ContextUnit.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.java.decompiler.struct;
 
 import net.fabricmc.fernflower.api.IFabricResultSaver;
@@ -69,7 +69,7 @@ public class ContextUnit {
 
       StructClass newCl;
       try (DataInputFullStream in = loader.getClassStream(oldName)) {
-        newCl = new StructClass(in, cl.isOwn(), loader);
+        newCl = StructClass.create(in, cl.isOwn(), loader);
       }
 
       lstClasses.add(newCl);
