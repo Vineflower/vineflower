Chocohead/ForgedFlower

Convert Exprent.bytecode to a BitMap.
It gives better speed and functionality. Added method to gather bytecode markers for every expression. Allows us to get the bytecode offsets for an entire block of code. Also more aggressively capture bytecode ranges to better understand how much of the code we are processing/outputting

src/org/jetbrains/java/decompiler/struct/StructMethod.java
@@ -313,11 +313,11 @@ public class StructMethod extends StructMember {
         }
       }
 
-      Instruction instr = Instruction.create(opcode, wide, group, bytecode_version, ops);
+      i++;
+
+      Instruction instr = Instruction.create(opcode, wide, group, bytecode_version, ops, i - offset);
 
       instructions.addWithKey(instr, offset);
-
-      i++;
     }
 
     // initialize exception table

Vineflower/vineflower

src/org/jetbrains/java/decompiler/struct/StructMethod.java
@@ -297,11 +297,11 @@ public class StructMethod extends StructMember {
         }
       }
 
-      Instruction instr = Instruction.create(opcode, wide, group, bytecodeVersion, ops);
+      i++;
+
+      Instruction instr = Instruction.create(opcode, wide, group, bytecodeVersion, ops, i - offset);
 
       instructions.addWithKey(instr, offset);
-
-      i++;
     }
 
     // initialize exception table