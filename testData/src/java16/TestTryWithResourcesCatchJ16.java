fba564f4f979d98ad9f146440994a4330aedcb76
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2021-06-21 23:32:18 -0400

    Add tests for java 16 try with resources

---
+++ testData/src/java16/TestTryWithResourcesCatchJ16.java
@@ -0,0 +1,27 @@
+package java16;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TestTryWithResourcesCatchJ16 {
+    public void test(File file) {
+        try (Scanner scanner = new Scanner(file)) {
+            scanner.next();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testFunc(File file) {
+        try (Scanner scanner = create(file)) {
+            scanner.next();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Scanner create(File file) throws FileNotFoundException {
+        return new Scanner(file);
+    }
+}
