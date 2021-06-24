c8fb180d4e019b734007b6ce8cca127b0321eceb
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2021-06-24 07:50:03 -0400

    Send warning if the name of the class file and the class within don't match

--- src/org/jetbrains/java/decompiler/struct/StructContext.java
+++ src/org/jetbrains/java/decompiler/struct/StructContext.java
@@ -184,8 +184,18 @@ public class StructContext {
     if (name == null || name.isEmpty()) {
       name = provider.get().qualifiedName;
     }
+
+    StructClass cl = provider.get();
+    String className = cl.qualifiedName.substring(cl.qualifiedName.lastIndexOf('/') + 1);
+    if (!className.equals(name)) {
+      DecompilerContext.getLogger().writeMessage("Inconsistent class name and class file! Expected " + className + ".class but found " + name + ".class!", Severity.WARN);
+
+      name = className;
+    }
+    // TODO: change the name of the created .java file too
+
     classes.put(name, provider);
-    if (isOwn) ownClasses.put(name, provider.get());
+    if (isOwn) ownClasses.put(name, cl);
   }
 
   public void addData(String path, String cls, byte[] data, boolean isOwn) throws IOException {
