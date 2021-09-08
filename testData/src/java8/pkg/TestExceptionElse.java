bba1b645a653d8526c4e539f3d44fab10a15f5b7
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2021-09-08 12:18:37 -0400

    Simplify TestExceptionElse

--- testData/src/java8/pkg/TestExceptionElse.java
+++ testData/src/java8/pkg/TestExceptionElse.java
@@ -10,17 +10,9 @@ public class TestExceptionElse {
       Number number = numbers.removeFirst();
       if (number instanceof Integer) {
         System.out.println(1);
-      } else if (numbers.size() == 4 && number instanceof Long) {
-        System.out.println(2);
-      } else if (number instanceof Double) {
-        System.out.println(3);
       } else if (numbers.size() == 3 && number instanceof Float) {
         System.out.println("here");
       } else {
-        if (numbers.size() == 0) {
-          System.out.println(4);
-        }
-
         throw new RuntimeException("Not a valid number");
       }
     }
