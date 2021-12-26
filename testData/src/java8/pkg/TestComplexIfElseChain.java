b4d5ef0a1786c89af3e3a2553ea2f7bad84a0f98
Joe <burtonjae@hotmail.co.uk>
2021-12-26 21:41:11 +0000

    Fix complex if-else chain

--- testData/src/java8/pkg/TestComplexIfElseChain.java
+++ testData/src/java8/pkg/TestComplexIfElseChain.java
@@ -3,19 +3,37 @@ package pkg;
 import java.util.Random;
 
 public class TestComplexIfElseChain {
-  public void foo() {
+  public void test() {
     Random randy = new Random();
     int result = randy.nextInt(11);
     if (result == 0 || result == 1) {
-        System.out.println("a");
+      System.out.println("a");
     } else if (result == 2 || result == 3) {
-        System.out.println("b");
+      System.out.println("b");
     } else if (result == 4 || result == 5) {
-        System.out.println("c");
+      System.out.println("c");
     } else if (result == 6 || result == 7) {
-        System.out.println("d");
+      System.out.println("d");
     } else if (result == 8 || result == 9) {
+      System.out.println("e");
+    }
+  }
+
+  public void testInLoop() {
+    Random randy = new Random();
+    int result = randy.nextInt(11);
+    for (int i = 0; i < 10; i++) {
+      if (result == 0 || result == 1) {
+        System.out.println("a");
+      } else if (result == 2 || result == 3) {
+        System.out.println("b");
+      } else if (result == 4 || result == 5) {
+        System.out.println("c");
+      } else if (result == 6 || result == 7) {
+        System.out.println("d");
+      } else if (result == 8 || result == 9) {
         System.out.println("e");
+      }
     }
   }
 }
