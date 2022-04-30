3ea79ef2abda3a7272a80f6ac6179c17ef933810
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2022-04-30 14:46:28 -0400

    More validation work, validate all types of edges and exprents in the statement

--- src/org/jetbrains/java/decompiler/modules/decompiler/ValidationHelper.java
+++ src/org/jetbrains/java/decompiler/modules/decompiler/ValidationHelper.java
@@ -1,6 +1,7 @@
 package org.jetbrains.java.decompiler.modules.decompiler;
 
 import org.jetbrains.java.decompiler.modules.decompiler.exps.ExitExprent;
+import org.jetbrains.java.decompiler.modules.decompiler.exps.Exprent;
 import org.jetbrains.java.decompiler.modules.decompiler.sforms.DirectGraph;
 import org.jetbrains.java.decompiler.modules.decompiler.sforms.DirectNode;
 import org.jetbrains.java.decompiler.modules.decompiler.sforms.FlattenStatementsHelper;
@@ -40,25 +41,48 @@ public final class ValidationHelper {
 
     for (Statement stat : statements) {
       for (StatEdge edge : stat.getAllSuccessorEdges()) {
-        if (!statements.contains(edge.getSource())) {
-          throw new IllegalStateException("Edge pointing from non-existing statement: [" + stat + "] " + edge);
-        }
+        validateEdge(statements, stat, edge);
+      }
 
-        if (!statements.contains(edge.getDestination())) {
-          throw new IllegalStateException("Edge pointing to non-existing statement: [" + stat + "] " + edge);
-        }
+      for (StatEdge edge : stat.getAllPredecessorEdges()) {
+        validateEdge(statements, stat, edge);
+      }
 
-        if (edge.closure != null) {
-          if (!statements.contains(edge.closure)) {
-            throw new IllegalStateException("Edge with non-existing closure: [" + stat + "] " + edge);
+      for (StatEdge edge : stat.getLabelEdges()) {
+        validateEdge(statements, stat, edge);
+      }
+
+      if (stat.getExprents() != null) {
+        for (Exprent exprent : stat.getExprents()) {
+          for (Exprent ex : exprent.getAllExprents(true, true)) {
+            if (ex.type == Exprent.EXPRENT_EXIT) {
+              ExitExprent exit = (ExitExprent)ex;
+              validateExitExprent(exit);
+            }
           }
         }
       }
     }
 
     FlattenStatementsHelper flatten = new FlattenStatementsHelper();
-    final DirectGraph directGraph = flatten.buildDirectGraph(statement);
+    DirectGraph directGraph = flatten.buildDirectGraph(statement);
     validateDGraph(directGraph, statement);
+  }
+
+  private static void validateEdge(VBStyleCollection<Statement, Integer> statements, Statement stat, StatEdge edge) {
+    if (!statements.contains(edge.getSource())) {
+      throw new IllegalStateException("Edge pointing from non-existing statement: [" + stat + "] " + edge);
+    }
+
+    if (!statements.contains(edge.getDestination())) {
+      throw new IllegalStateException("Edge pointing to non-existing statement: [" + stat + "] " + edge);
+    }
+
+    if (edge.closure != null) {
+      if (!statements.contains(edge.closure)) {
+        throw new IllegalStateException("Edge with non-existing closure: [" + stat + "] " + edge);
+      }
+    }
   }
 
   public static void validateDGraph(DirectGraph graph, RootStatement root) {
@@ -111,7 +135,7 @@ public final class ValidationHelper {
     }
 
     if (o == null) {
-      throw new NullPointerException("Validation: null object");
+      throw new NullPointerException("Validation: null object: " + o);
     }
   }
 
@@ -119,15 +143,15 @@ public final class ValidationHelper {
     if (!VALIDATE) {
       return;
     }
-    
+
     if (exit.getExitType() == ExitExprent.EXIT_RETURN) {
       if (exit.getRetType().equals(VarType.VARTYPE_VOID)){
         if (exit.getValue() != null) {
-          throw new IllegalStateException("Void return with value");
+          throw new IllegalStateException("Void return with value: " + exit);
         }
       } else {
         if (exit.getValue() == null) {
-          throw new IllegalStateException("Non-void return without value");
+          throw new IllegalStateException("Non-void return without value: " + exit);
         }
       }
     }
