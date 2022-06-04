d8beb08d0d18b2465fc08526d43be48054836a68
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2022-06-04 17:10:32 +0200

    Add yet another failing switch pattern matching case

--- test/org/jetbrains/java/decompiler/SingleClassesTest.java
+++ test/org/jetbrains/java/decompiler/SingleClassesTest.java
@@ -434,6 +434,7 @@ public class SingleClassesTest extends SingleClassesTestBase {
     register(JAVA_17_PREVIEW, "TestSwitchPatternMatching19");
     register(JAVA_17_PREVIEW, "TestSwitchPatternMatching20");
     register(JAVA_17_PREVIEW, "TestSwitchPatternMatching21");
+    register(JAVA_17_PREVIEW, "TestSwitchPatternMatching22");
 
     register(JAVA_17_PREVIEW, "TestSwitchPatternMatchingLoop");
     register(JAVA_17_PREVIEW, "TestSwitchPatternMatchingInstanceof1");
