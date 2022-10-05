84d671d926210d16a7fa445e0239f4da78edce87
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-10-05 16:43:52 +0300

    Add extra case for int[][]

--- testData/src/java8/pkg/TestArrayConstructorReference.java
+++ testData/src/java8/pkg/TestArrayConstructorReference.java
@@ -4,6 +4,7 @@ import java.util.function.IntFunction;
 
 class TestArrayConstructorReference {
   static final IntFunction<int[]> INT = int[]::new;
+  static final IntFunction<int[][]> INT_ARRAY = int[][]::new;
   static final IntFunction<String[]> STRING = String[]::new;
   static final IntFunction<String[][]> STRING_ARRAY = String[][]::new;
 }
