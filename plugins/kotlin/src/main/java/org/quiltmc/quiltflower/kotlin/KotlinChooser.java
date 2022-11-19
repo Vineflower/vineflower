64ebf581909fe758891faadf602455b98c1513e7
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2022-11-19 01:40:04 -0500

    Kotlin: Fix metadata parsing

--- plugins/kotlin/src/main/java/org/quiltmc/quiltflower/kotlin/KotlinChooser.java
+++ plugins/kotlin/src/main/java/org/quiltmc/quiltflower/kotlin/KotlinChooser.java
@@ -1,10 +1,10 @@
 package org.quiltmc.quiltflower.kotlin;
 
-import com.google.protobuf.ByteString;
-import com.google.protobuf.ExtensionRegistryLite;
-import com.google.protobuf.InvalidProtocolBufferException;
+import kotlin.reflect.jvm.internal.impl.metadata.ProtoBuf;
+import kotlin.reflect.jvm.internal.impl.metadata.jvm.JvmProtoBuf;
+import kotlin.reflect.jvm.internal.impl.protobuf.ExtensionRegistryLite;
 import org.jetbrains.java.decompiler.api.LanguageChooser;
-import org.jetbrains.java.decompiler.api.metadata.BitEncoding;
+import org.quiltmc.quiltflower.kotlin.metadata.BitEncoding;
 import org.jetbrains.java.decompiler.modules.decompiler.exps.AnnotationExprent;
 import org.jetbrains.java.decompiler.modules.decompiler.exps.ConstExprent;
 import org.jetbrains.java.decompiler.modules.decompiler.exps.Exprent;
@@ -12,14 +12,9 @@ import org.jetbrains.java.decompiler.modules.decompiler.exps.NewExprent;
 import org.jetbrains.java.decompiler.struct.StructClass;
 import org.jetbrains.java.decompiler.struct.attr.StructAnnotationAttribute;
 import org.jetbrains.java.decompiler.struct.attr.StructGeneralAttribute;
-import org.jetbrains.java.decompiler.util.TextUtil;
-import org.jetbrains.kotlin.metadata.ProtoBuf;
-import org.jetbrains.kotlin.metadata.jvm.JvmProtoBuf;
+import org.quiltmc.quiltflower.kotlin.metadata.MetadataNameResolver;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 
 public class KotlinChooser implements LanguageChooser {
   private static final StructGeneralAttribute.Key<?>[] ANNOTATION_ATTRIBUTES = {
@@ -41,17 +36,28 @@ public class KotlinChooser implements LanguageChooser {
         for (AnnotationExprent anno : attr.getAnnotations()) {
           if (anno.getClassName().equals("kotlin/Metadata")) {
 
-            Exprent ex = anno.getParValues().get(3);
-            NewExprent newEx = (NewExprent) ex;
-            // TODO: all elements
-            String value = (String) ((ConstExprent) newEx.getLstArrayElements().get(0)).getValue();
-//            System.out.println(ConstExprent.convertStringToJava(value, false));
+            Exprent d1 = anno.getParValues().get(3);
+            Exprent d2 = anno.getParValues().get(4);
+
+            String[] data1 = getDataFromExpr((NewExprent) d1);
+
 
             try {
-              byte[] buf = BitEncoding.decodeBytes(new String[]{value});
-//              System.out.println(Arrays.toString(buf));
-              // FIXME: instantly crashes!
-//              ProtoBuf.Class pcl = ProtoBuf.Class.parseFrom(new ByteArrayInputStream(buf), EXTENSIONS);
+              String[] data2 = getDataFromExpr((NewExprent) d2);
+
+              byte[] buf = BitEncoding.decodeBytes(data1);
+
+              ByteArrayInputStream input = new ByteArrayInputStream(buf);
+              JvmProtoBuf.StringTableTypes types = JvmProtoBuf.StringTableTypes.parseDelimitedFrom(input, EXTENSIONS);
+              // TODO: use kind to figure out which to parse
+              ProtoBuf.Class pcl = ProtoBuf.Class.parseFrom(input, EXTENSIONS);
+
+              MetadataNameResolver resolver = new MetadataNameResolver(types, data2);
+//              System.out.println(resolver.resolve(pcl.getFqName()));
+//
+//              for (ProtoBuf.Function func : pcl.getFunctionList()) {
+//                System.out.println(resolver.resolve(func.getName()));
+//              }
             } catch (Exception e) {
               e.printStackTrace();
             }
@@ -62,5 +68,14 @@ public class KotlinChooser implements LanguageChooser {
       }
     }
     return false;
+  }
+
+  private String[] getDataFromExpr(NewExprent d2) {
+    return d2.getLstArrayElements()
+      .stream()
+      .map(ConstExprent.class::cast)
+      .map(ConstExprent::getValue)
+      .map(String.class::cast)
+      .toArray(String[]::new);
   }
 }
