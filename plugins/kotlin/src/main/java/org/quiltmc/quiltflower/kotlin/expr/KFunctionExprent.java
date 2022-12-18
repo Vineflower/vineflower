cb7e3ac3b8f110a6a99ebf03a5fa3474f833efd5
sschr15 <steven.sschr15@hotmail.com>
2022-12-18 15:18:22 -0600

    `!is` and bitwise functions

--- plugins/kotlin/src/main/java/org/quiltmc/quiltflower/kotlin/expr/KFunctionExprent.java
+++ plugins/kotlin/src/main/java/org/quiltmc/quiltflower/kotlin/expr/KFunctionExprent.java
@@ -46,6 +46,44 @@ public class KFunctionExprent extends FunctionExprent {
         }
         buf.append(wrapOperandString(lstOperands.get(0), true, indent)).append(" as ").append(lstOperands.get(1).toJava(indent));
         return buf;
+      case BIT_NOT:
+        buf.append(wrapOperandString(lstOperands.get(0), true, indent));
+        return buf.append(".inv()");
+      case AND: // Bitwise AND
+        buf.append(wrapOperandString(lstOperands.get(0), true, indent)).append(" and ")
+          .append(wrapOperandString(lstOperands.get(1), true, indent));
+        return buf;
+      case OR:
+        buf.append(wrapOperandString(lstOperands.get(0), true, indent)).append(" or ")
+          .append(wrapOperandString(lstOperands.get(1), true, indent));
+        return buf;
+      case XOR:
+        buf.append(wrapOperandString(lstOperands.get(0), true, indent)).append(" xor ")
+          .append(wrapOperandString(lstOperands.get(1), true, indent));
+        return buf;
+      case SHL:
+        buf.append(wrapOperandString(lstOperands.get(0), true, indent)).append(" shl ")
+          .append(wrapOperandString(lstOperands.get(1), true, indent));
+        return buf;
+      case SHR:
+        buf.append(wrapOperandString(lstOperands.get(0), true, indent)).append(" shr ")
+          .append(wrapOperandString(lstOperands.get(1), true, indent));
+        return buf;
+      case USHR:
+        buf.append(wrapOperandString(lstOperands.get(0), true, indent)).append(" ushr ")
+          .append(wrapOperandString(lstOperands.get(1), true, indent));
+        return buf;
+      case BOOL_NOT:
+        // Special cases for `is` and `!is`
+        // TODO: do the same for `in` and `!in`
+        if (lstOperands.get(0) instanceof KFunctionExprent) {
+          KFunctionExprent func = (KFunctionExprent) lstOperands.get(0);
+          if (func.getFuncType() == FunctionExprent.FunctionType.INSTANCEOF) {
+            TextBuffer buf2 = func.toJava(indent);
+            buf.append(buf2.convertToStringAndAllowDataDiscard().replace(" is ", " !is "));
+            return buf;
+          }
+        }
     }
 
     return buf.append(super.toJava(indent));
