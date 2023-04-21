d78d5fc2f062eb36b4f0e900ad3f7597770b0cf5
ix0rai <ix0rai64@gmail.com>
2023-04-21 18:34:18 -0500

    fix improper offset with generics (#264)
    * properly account for generic offset
    
    * update TestTextTokens2

--- testData/src/java16/pkg/TestTextTokens2.java
+++ testData/src/java16/pkg/TestTextTokens2.java
@@ -4,6 +4,7 @@ import ext.ExampleAnnotation;
 
 import java.util.Optional;
 import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public record TestTextTokens2<T>(String name, @ExampleAnnotation T value, int index, Object... args) {
@@ -14,5 +15,9 @@ public record TestTextTokens2<T>(String name, @ExampleAnnotation T value, int in
   public void bar(Supplier<Optional<? extends BiConsumer<T, String>>> r) {
     String s = "Hello world";
     r.get().ifPresent(c -> c.accept(value, s));
+  }
+
+  public Function<String, String> baz() {
+    return s -> s + " " + this.name;
   }
 }
