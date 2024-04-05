2e4165f73f6621c240f16abb5f19a3ae3a0023fb
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2024-04-06 00:40:40 +0200

    Fix copying values in arrays

--- test/org/jetbrains/java/decompiler/SingleClassesTest.java
+++ test/org/jetbrains/java/decompiler/SingleClassesTest.java
@@ -696,6 +696,7 @@ public class SingleClassesTest extends SingleClassesTestBase {
     register(JAVA_21, "TestSwitchOnEnumJ21", "ext/TestEnum2");
     register(JAVA_21, "TestInnerClassesJ21");
     register(JAVA_8, "TestInnerClassesJ8");
+    register(JAVA_8, "TestSwitchInTry");
   }
 
   private void registerEntireClassPath() {
