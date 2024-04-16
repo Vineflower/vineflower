df038e86fda1a51fe10c0f1a36c60535d692ccf4
sschr15 <steven.sschr15@hotmail.com>
2024-04-16 12:45:19 -0500

    Describe in detail what happens to default parameters

--- plugins/kotlin/src/main/java/org/vineflower/kotlin/struct/DefaultArgsMap.java
+++ plugins/kotlin/src/main/java/org/vineflower/kotlin/struct/DefaultArgsMap.java
@@ -20,11 +20,44 @@ public class DefaultArgsMap {
   private DefaultArgsMap(Map<KParameter, Exprent> map) {
     this.map = map;
   }
-
   public static DefaultArgsMap from(MethodWrapper defaults, MethodWrapper calling, KParameter[] params) {
     if (defaults == null) {
       return new DefaultArgsMap(Map.of());
     }
+
+    /*
+     * Kotlin's method to define default arguments is a bit odd. The default values are stored in a separate method
+     * which is called in place of the actual method when defaults are needed. The method containing the defaults
+     * contains two extra arguments, a bitmask and a value that is always null. The bitmask specifies which arguments
+     * should be replaced with defaults, and it corresponds one-to-one with all the parameters of the actual method,
+     * including non-defaulted ones. Constructors follow the same pattern, but they include an extra parameter
+     * (also always null) to differentiate them from actual constructors.
+     *
+     * In the case of 32 or more parameters, additional bitmask fields are created as needed.
+     *
+     * For a method defined as `fun foo(arg1: Int = 42, arg2: String, arg3: Any? = null)`,
+     * the method containing its default values would look like this:
+     *
+     * String foo$default(int arg1, String arg2, Object arg3, int bitmask, Object alwaysNull) {
+     *   if ((bitmask & 0b001) != 0) {
+     *    arg1 = 42;
+     *   }
+     *
+     *   if ((bitmask & 0b100) != 0) {
+     *     arg3 = null;
+     *   }
+     *
+     *   return foo(arg1, arg2, arg3);
+     * }
+     *
+     * To parse this, the method graph is traversed, and for each if statement, the condition of the statement is
+     * checked to see if it matches the pattern Kotlin uses, and the if-true branch is checked to see if it is a
+     * simple assignment. If both are true, it assumes that the assignment is a default value check and extracts
+     * the assigned value.
+     *
+     * This can get most defaults, but it may fail with very complex defaults, such as a `run` block or other
+     * inlined blocks.
+     */
 
     Map<KParameter, Exprent> map = new HashMap<>();
 
@@ -33,14 +66,20 @@ public class DefaultArgsMap {
       Statement statement = node.statement;
       if (statement instanceof IfStatement ifStatement) {
         Exprent condition = ifStatement.getHeadexprent().getCondition();
-        if (!(condition instanceof FunctionExprent function)) continue;
-        if (!(function.getLstOperands().get(0) instanceof FunctionExprent bitmask)) continue;
-        if (bitmask.getLstOperands().size() != 2) continue;
+        if (
+          !(condition instanceof FunctionExprent function) ||
+          !(function.getLstOperands().get(0) instanceof FunctionExprent bitmask) ||
+          bitmask.getLstOperands().size() != 2
+        ) {
+          continue;
+        }
 
         Exprent var = bitmask.getLstOperands().get(0);
         Exprent mask = bitmask.getLstOperands().get(1);
 
-        if (!(var instanceof VarExprent) || !(mask instanceof ConstExprent)) continue;
+        if (!(var instanceof VarExprent) || !(mask instanceof ConstExprent)) {
+          continue;
+        }
 
         int maskValue = ((ConstExprent) mask).getIntValue();
         int maskIndex = 0;
