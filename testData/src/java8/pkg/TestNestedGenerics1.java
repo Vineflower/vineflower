a0d5c2058abc02e92c634bcdbaf4392e53abd98c
sschr15 <steven.sschr15@hotmail.com>
2025-10-29 23:35:46 -0500

    Fix generic erasure for lambdas

--- testData/src/java8/pkg/TestNestedGenerics1.java
+++ testData/src/java8/pkg/TestNestedGenerics1.java
@@ -1,6 +1,7 @@
 package pkg;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class TestNestedGenerics1 {
   public static <T, R> R test(T t) {
@@ -11,5 +12,16 @@ public class TestNestedGenerics1 {
       }
     };
     return instance.apply(t);
+  }
+  
+  public static <T, R> R testInLambda(T t, R r) {
+    Supplier<Function<T, R>> functionMaker = () -> new Function<T, R>() {
+      @Override
+      public R apply(T t) {
+        return r;
+      }
+    };
+
+    return functionMaker.get().apply(t);
   }
 }
