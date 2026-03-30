c8781a0a8464314ef9e879933721bf85d1f301a5
aoqia <aoqia@aoqia.dev>
2026-03-30 15:12:12 +1100

    chore: Move changes over from feat/string-switch-rewrite

--- src/org/jetbrains/java/decompiler/util/collections/VBStyleCollection.java
+++ src/org/jetbrains/java/decompiler/util/collections/VBStyleCollection.java
@@ -29,8 +29,13 @@ public class VBStyleCollection<E, K> extends ArrayList<E> {
   }
 
   @Override
-  public boolean remove(Object element) {   // TODO: error on void remove(E element)
-    throw new RuntimeException("not implemented!");
+  public boolean remove(Object element) {
+    final var index = this.indexOf(element);
+    if (index == -1) {
+      return false;
+    }
+
+    return this.remove(index) != null;
   }
 
   @Override
