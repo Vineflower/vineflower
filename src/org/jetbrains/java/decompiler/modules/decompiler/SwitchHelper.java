abd43944a857f4d7e6971d1e7bd6970c5e13df77
aoqia <aoqia@aoqia.dev>
2026-06-26 09:03:36 +1000

    style: fix formatting again

--- src/org/jetbrains/java/decompiler/modules/decompiler/SwitchHelper.java
+++ src/org/jetbrains/java/decompiler/modules/decompiler/SwitchHelper.java
@@ -605,7 +605,7 @@ public final class SwitchHelper {
    * @return a case map with desired values if found, otherwise an empty map
    */
   private static List<List<Exprent>> getStringSwitchRealCaseValues(StringSwitch switchInfo,
-                                                                   HashMap<Integer, List<Exprent>> caseMap) {
+    HashMap<Integer, List<Exprent>> caseMap) {
     List<List<Exprent>> realCaseValues = new ArrayList<>();
 
     for (int i = 0; i < switchInfo.target().getCaseValues().size(); i++) {
